c44f904fdbea44c4feb9437470a6b498d10de380
Joshua <jcmathews2@wisc.edu>
2019-10-25 19:53:51 -0500

    Add horizontal and vertical movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 10f;
+    public float maxSpeed = 6f;
+    public LayerMask groundLayer;
+    public float jumpForce = 1200.0f;
+    public CapsuleCollider collider;
+    public float distToGround;
+    public float distToSides;
+    public float gravity = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        collider = GetComponent<CapsuleCollider>();
+        distToGround = collider.bounds.extents.y;
+        distToSides = collider.bounds.extents.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0, 0);
+        Debug.Log("" + Input.GetAxis("Vertical"));
+
+        if (IsGrounded())
+        {
+            if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                rb.AddForce(Vector3.up * jumpForce);
+            }
+
+        
+        }
+        rb.AddForce(Vector3.down * gravity, ForceMode.VelocityChange);
+
+        // rb.velocity.y -= gravity * Time.deltaTime;
+
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.01f);
+    }
+}
---
+++ Assets/playerControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 3f;
+    public float maxSpeed = 6f;
+    public LayerMask groundLayer;
+    public float jumpForce = 2f;
+    public CapsuleCollider collider;
+    public float distToGround;
+    public float distToSides;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        collider = GetComponent<CapsuleCollider>();
+        distToGround = collider.bounds.extents.y;
+        distToSides = collider.bounds.extents.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (rb.velocity.x > -maxSpeed)
+            {
+                rb.AddForce(Vector3.left * speed, ForceMode.Impulse);
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (rb.velocity.x< maxSpeed)
+            {
+                rb.AddForce(Vector3.right * speed, ForceMode.Impulse);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (IsGrounded())
+            {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            }
+        }
+
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+}
