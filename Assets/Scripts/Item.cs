407cc4a06069c8f760963f99b60da0523a42b509
rhyane <bhart2055@gmail.com>
2019-11-01 12:06:40 -0500

    simple items scripts
    I need  a UI so I can make these functional and write the use functions

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    public string name;
+    public Sprite image;
+    public string description;
+
+    public virtual void use()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Weapon", menuName = "Item/Equipment/Weapon")]
+public class Weapon : Equipment
+{
+    
+   
+}
