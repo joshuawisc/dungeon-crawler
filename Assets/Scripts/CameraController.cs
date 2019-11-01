025ec26d22efddae0a2c5d9a3c46a9dd09ce3e3e
unknown <axu1672@gmail.comm>
2019-10-31 22:44:50 -0500

    added CameraController

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform lookAt;
+    public Transform cameraTransform;
+    private Camera camera;
+    private float cameraDistance = 15.0f ;
+    
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        cameraTransform = transform;
+        camera = Camera.main;
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 direction = new Vector3(0 , 0 , -cameraDistance);
+        
+        cameraTransform.position = lookAt.position + direction;
+
+    }
+}
