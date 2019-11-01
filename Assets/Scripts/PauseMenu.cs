0c0971d0c57ec073566ee3a64d8ffa35408b406e
calebwilson33 <56949567+calebwilson33@users.noreply.github.com>
2019-11-01 11:15:41 -0500

    UI and State
    Updated UI and state settings

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    // Update is called once per frame
+   
+
+    public static void Resume(GameObject pauseMenuUI)
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public static void Pause(GameObject pauseMenuUI)
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeClick(GameObject pauseMenuUI)
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Loading Menu...");
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void LoadOptions()
+    {
+        Debug.Log("Loading Options...");
+        //SceneManager.LoadScene("Options");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
