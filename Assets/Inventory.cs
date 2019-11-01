b0ccb67ffe56c1eff08b462b08e779fe2f1a1ffa
rhyane <bhart2055@gmail.com>
2019-11-01 17:15:06 -0500

    Add files via upload
    Should work with master if tested refer to https://www.mvcode.com/lessons/unity-rpg-inventory-system-jamie  for help

---
+++ Assets/Inventory.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public List<Item> list = new List<Item>();
+    public GameObject player;
+    public GameObject InventoryPanel;
+    public static Inventory instance;
+
+    void updatePanelSlots()
+    {
+       int index = 0;
+        foreach(Transform child in InventoryPanel.transform)
+        {
+            InventorySlotController slot = child.GetComponent<InventorySlotController>();
+
+            if(index < list.Count)
+            {
+                slot.item = list[index];
+            }
+            else
+            {
+                slot.item = null;
+            }
+            slot.UpdateInfo();
+            index++;
+        }
+    }
+    void Start()
+    {
+        instance = this;
+        updatePanelSlots(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Add(Item item)
+    {
+        if (list.Count < 1) {
+            list.Add(item);
+        }
+        updatePanelSlots();
+    }
+
+    public void Remove(Item item)
+    {
+        list.Remove(item);
+        updatePanelSlots();
+    }
+}
---
+++ Assets/InventorySlotController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlotController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Item item;
+
+    public void use()
+    {
+        if (item)
+        {
+            Debug.Log("You Clicked" + item.name);
+        }
+    }
+
+    public void UpdateInfo()
+    {
+        Text displayText = transform.Find("Text").GetComponent<Text>();
+        Image displayImage = transform.Find("Image").GetComponent<Image>();
+
+        if (item)
+        {
+            displayText.text = item.name;
+            displayImage.sprite = item.image;
+            displayImage.color = Color.white;
+        }
+
+        else
+        {
+            displayText.text = "";
+            displayImage.sprite = null;
+        }
+    }
+
+    private void Start()
+    {
+        UpdateInfo();
+        
+    }
+}
