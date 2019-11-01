7ba1caf120a04446b113f6780e3b03aec463b3c1
unknown <axu1672@gmail.comm>
2019-11-01 17:11:55 -0500

    adding Caleb's UI to Zhuyan's Basic Map

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     public void StartGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("BasicMap10.31");
     }
 
     public void LoadGame()
